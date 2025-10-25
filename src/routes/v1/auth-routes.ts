74c1b0f70ac1bab1a3756cd0909e04407cb990c6
shibujoseph <shibujoseph1815@gmail.com>
2025-10-25 19:52:50 +0530

    routes setup

---
+++ src/routes/v1/auth-routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  registerController,
+  loginController,
+} from "../../controllers/auth-controller.js";
+import { validatePayload } from "../../middlewares/input-validation-middleware.js";
+import { registerSchema, loginSchema } from "../../types/auth-types.js";
+// import { user } from "../../controllers/user-controller.js";
+
+const authRouter = Router();
+
+authRouter.post(
+  "/register",
+  validatePayload(registerSchema),
+  registerController
+);
+authRouter.post("/login", validatePayload(loginSchema), loginController);
+
+export default authRouter;
