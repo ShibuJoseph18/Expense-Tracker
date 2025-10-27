039bd896a02a1310609456501662183efc5d237a
shibujoseph <shibujoseph1815@gmail.com>
2025-10-27 16:54:29 +0530

    add sqlite db

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -1,10 +1,22 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-import config from "./config.js";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Create better-sqlite3 instance
-const sqlite = new Database(config.dbFile, { verbose: () => {} });
+const dbFilePath = path.join(__dirname, "..", "db", "sqlite.db");
 
-// Wrap with Drizzle
-const db = drizzle(sqlite);
-export default db;
+export let db: any;
+(async () => {
+  db = await open({
+    filename: dbFilePath,
+    driver: sqlite3.cached.Database,
+  });
+
+  const sqlPath = path.join(__dirname, "..", "db", "tables.sql");
+  const sql = fs.readFileSync(sqlPath, "utf8");
+  await db.exec(sql);
+  console.log("✅ Tables created successfully");
+})();
