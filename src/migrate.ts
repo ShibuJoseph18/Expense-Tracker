075d2c94b99f7cc70fe2a879a08c2302826bca17
shibujoseph <shibujoseph1815@gmail.com>
2025-10-27 16:52:30 +0530

    remove drizzle orm

--- src/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import db from "./config/db-config.js";
-
-export function initDB() {
-  console.log("Running migrations 🔃");
-  migrate(db, { migrationsFolder: "./drizzle" });
-  console.log("Migrations complete ✅");
-}
