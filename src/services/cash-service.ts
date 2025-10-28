ac45253d505de6dae751ac50c64e983cafd3e7a3
shibujoseph <shibujoseph1815@gmail.com>
2025-10-28 17:14:31 +0530

    add cash service

---
+++ src/services/cash-service.ts
@@ -0,0 +1,21 @@
+import { db } from "../config/db-config.js";
+
+export const intializeCashBalanceService = async (
+  userId: number
+): Promise<boolean> => {
+  try {
+    const initializeCashBalance = await db.run(
+      `INSERT INTO cash_balances (user_id) VALUES ($userId)`,
+      {
+        $userId: userId,
+      }
+    );
+    if (initializeCashBalance.changes && initializeCashBalance.changes > 0) {
+      return true;
+    }
+
+    return false;
+  } catch (err) {
+    return false;
+  }
+};
