38047b78089fef6158350689fd353da1a5e641f7
shibujoseph <shibujoseph1815@gmail.com>
2025-10-31 16:56:25 +0530

    redefine account types

--- src/types/account-types.ts
+++ src/types/account-types.ts
@@ -1,12 +1,40 @@
 import * as z from "zod";
 
-export const accountSchema = z.object({
+export const accountUserInputSchema = z.object({
   name: z.string(),
-  bankName: z.string().optional(),
-  accountNumber: z.string().optional(),
+  bank_name: z.string().optional(),
+  account_number: z.string().optional(),
   balance: z.number().optional(),
 });
 
-export type accountType = z.infer<typeof accountSchema> & {
-  initialDeposit?: number;
+export type accountType = {
+  id: number;
+  user_id: number;
+  name: string;
+  bank_name: string;
+  account_number: string;
+  balance: number;
+  initial_deposit: 0 | 1;
+  created_at: Date;
+  updated_at: Date;
 };
+
+export type accountServiceInputType = Pick<
+  accountType,
+  "name" | "bank_name" | "account_number" | "balance"
+>;
+
+export type accountServiceOutputType = Pick<
+  accountType,
+  "user_id" | "name" | "bank_name" | "account_number" | "balance"
+>;
+
+export type accountRepoInputType = Pick<
+  accountType,
+  | "user_id"
+  | "name"
+  | "bank_name"
+  | "account_number"
+  | "balance"
+  | "initial_deposit"
+>;
