369384372ab5b533b5dabb83256ac386d01c12d2
shibujoseph <shibujoseph1815@gmail.com>
2025-11-09 21:42:48 +0530

    ddl changes

--- src/db/sql/tables.sql
+++ src/db/sql/tables.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     mobile TEXT UNIQUE NOT NULL,
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
@@ -16,6 +17,7 @@ CREATE TABLE IF NOT EXISTS accounts (
     account_number TEXT UNIQUE,
     balance REAL DEFAULT 0,
     initial_deposit BOOLEAN DEFAULT 0,
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(user_id, name),
@@ -23,9 +25,11 @@ CREATE TABLE IF NOT EXISTS accounts (
 );
 
 CREATE TABLE IF NOT EXISTS cash_balances (
-    user_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER UNIQUE,
     initial_deposit BOOLEAN DEFAULT 0,
     balance REAL DEFAULT 0,
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -36,17 +40,19 @@ CREATE TABLE IF NOT EXISTS categories (
     name TEXT NOT NULL,
     type TEXT CHECK (type IN ('expense', 'income')) NOT NULL,
     is_global BOOLEAN DEFAULT 1,      -- 1 = system category, 0 = user-defined
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS user_categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    is_hidden BOOLEAN DEFAULT 0,          -- user can hide any category (global or custom)
+    category_id INTEGER NOT NULL,          -- user can hide any category (global or custom)
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, category_id),
+    UNIQUE (user_id, category_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
@@ -56,18 +62,20 @@ CREATE TABLE IF NOT EXISTS subcategories (
     category_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     is_global BOOLEAN DEFAULT 1,
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE IF NOT EXISTS user_subcategories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     subcategory_id INTEGER NOT NULL,
-    is_hidden BOOLEAN DEFAULT 0,
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, subcategory_id),
+    UNIQUE (user_id, subcategory_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (subcategory_id) REFERENCES subcategories(id)
 );
@@ -83,13 +91,13 @@ CREATE TABLE IF NOT EXISTS transactions (
     subcategory_id INTEGER,
     note TEXT,
     date DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    deleted BOOLEAN DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    deleted BOOLEAN DEFAULT 0,
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(account_id) REFERENCES accounts(id),
-    FOREIGN KEY(category_id) REFERENCES category(id),
-    FOREIGN KEY(subcategory_id) REFERENCES subcategory(id),
+    FOREIGN KEY(category_id) REFERENCES categories(id),
+    FOREIGN KEY(subcategory_id) REFERENCES subcategories(id),
     CHECK (
         (is_cash = 1 AND account_id IS NULL) OR
         (is_cash = 0 AND account_id IS NOT NULL)
