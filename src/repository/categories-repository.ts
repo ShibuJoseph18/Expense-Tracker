46607e893593bb04e10e4f55645c59715fa47a17
shibujoseph <shibujoseph1815@gmail.com>
2025-11-19 17:52:54 +0530

    initiate categories repo

---
+++ src/repository/categories-repository.ts
@@ -0,0 +1,19 @@
+import { db } from "../config/db-config.js";
+
+export const intializeDefaultCategories = async (userId: number) => {
+  try {
+    const insertedCategories = await db.run(
+      `INSERT INTO user_categories (user_id, category_id)
+     SELECT $userId, id
+     FROM categories
+     WHERE is_global = 1`,
+      { $userId: userId }
+    );
+    if (insertedCategories.changes && insertedCategories.changes > 0) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
