bd5f4f237fb0c62a261625595b89707d5b3f1dd4
shibujoseph <shibujoseph1815@gmail.com>
2025-11-19 17:54:09 +0530

    add detailed auth types

--- src/types/auth-types.ts
+++ src/types/auth-types.ts
@@ -1,15 +1,29 @@
 import * as z from "zod";
+import type { UserType } from "./user-types.js";
 
 export const registerSchema = z.object({
   name: z.string().regex(/^[A-Za-z ]+$/, "Name must contain only letters"),
   email: z.email(),
   password: z.string(),
-  mobile: z.string(),
+  mobile: z.string().optional(),
 });
-export type RegisterType = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
   email: z.email(),
   password: z.string().min(2).max(25),
 });
-export type LoginType = z.infer<typeof loginSchema>;
+
+export type RegisterServiceInputType = Pick<
+  UserType,
+  "name" | "email" | "password" | "mobile"
+>;
+
+export type RegisterServiceOutputType = Pick<
+  UserType,
+  "name" | "email" | "mobile"
+>;
+
+export type LoginServiceInputType = Pick<UserType, "email" | "password">;
+
+export type LoginServiceOutputType = { accessToken: string };
+
