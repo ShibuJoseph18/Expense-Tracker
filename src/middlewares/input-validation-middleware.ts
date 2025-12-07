7c7f6c5f6a2b619a10e1f1b1e41473a2e91679af
shibujoseph <shibujoseph1815@gmail.com>
2025-12-07 12:06:17 +0530

    replace ZodObject with ZodType for supporting discriminated union apart from objects

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -1,10 +1,9 @@
-import { ZodObject } from "zod";
+import { ZodType } from "zod";
 import type { Request, Response, NextFunction } from "express";
 import { ValidationError } from "../utils/errors/validation-error.js";
 
 export const validatePayload =
-  (zodSchema: ZodObject) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  (zodSchema: ZodType) => (req: Request, res: Response, next: NextFunction) => {
     const validateInput = zodSchema.safeParse(req.body);
     if (validateInput.error) {
       throw new ValidationError();
