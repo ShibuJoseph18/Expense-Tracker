3a17e4b564f1425333c76c886c5af9ad6468697a
shibujoseph <shibujoseph1815@gmail.com>
2025-12-07 12:53:08 +0530

    add cash-balance supporting functions

--- src/repository/cash_balances-repository.ts
+++ src/repository/cash_balances-repository.ts
@@ -1,4 +1,6 @@
 import { db } from "../config/db-config.js";
+import { ServerError } from "../utils/errors/server-error.js";
+import type { CashBalanceType } from "../types/cash_balance-types.js";
 
 export const intializeCashBalance = async (
   userId: number
@@ -47,4 +49,58 @@ export const initialDepositCashRepository = async (
   );
 
   return true;
+};
+
+export const getCashBalance = async (
+  userId: number
+): Promise<CashBalanceType> => {
+  const balance = await db.get(
+    `SELECT * FROM cash_balances WHERE user_id = $user_id AND deleted = 0`,
+    {
+      $user_id: userId,
+    }
+  );
+  return balance;
+};
+
+export const updateCashBalance = async (
+  userId: number,
+  amount: number
+): Promise<CashBalanceType> => {
+  const updateBalance = await db.run(
+    `
+    UPDATE cash_balances
+    SET balance = balance + $balance, updated_at = CURRENT_TIMESTAMP
+    WHERE user_id = $user_id AND deleted = 0
+    `,
+    {
+      $user_id: userId,
+      $balance: amount,
+    }
+  );
+
+  if (!updateBalance.changes) {
+    throw new ServerError("Cash balance update failed");
+  }
+
+  const updatedBalance = await getCashBalance(userId);
+  return updatedBalance;
+};
+
+export const negativeCashBalance = async (
+  userId: number,
+  amount: number
+): Promise<Boolean> => {
+  const balance = await db.get(
+    `SELECT balance FROM cash_balances WHERE user_id = $user_id`,
+    {
+      $user_id: userId,
+    }
+  );
+
+  const newBalance = balance.balance - amount;
+  if (newBalance < 0) {
+    return true;
+  }
+  return false;
 };
