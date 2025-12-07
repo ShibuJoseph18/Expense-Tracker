cfaf4f1fc333ee8716994bfc3f71c255b7facea1
shibujoseph <shibujoseph1815@gmail.com>
2025-12-07 13:04:39 +0530

    modify transaction types

--- src/types/transaction-types.ts
+++ src/types/transaction-types.ts
@@ -1,9 +1,19 @@
 import * as z from "zod";
 
-export const transactionSchema = z.object({
+export const cashTransactionSchema = z.object({
   type: z.enum(["expense", "income"]),
   amount: z.number(),
-  is_cash: z.union([z.literal(0), z.literal(1)]),
+  is_cash: z.literal(1),
+  category_id: z.number(),
+  subcategory_id: z.number().optional(),
+  note: z.string().optional(),
+  date: z.date().optional().nullable(),
+});
+
+export const accountTransactionSchema = z.object({
+  type: z.enum(["expense", "income"]),
+  amount: z.number(),
+  is_cash: z.literal(0),
   account_id: z.number(),
   category_id: z.number(),
   subcategory_id: z.number().optional(),
@@ -11,7 +21,12 @@ export const transactionSchema = z.object({
   date: z.date().optional().nullable(),
 });
 
-export type transactionType = {
+export const transactionSchema = z.discriminatedUnion("is_cash", [
+  accountTransactionSchema,
+  cashTransactionSchema,
+]);
+
+export type TransactionType = {
   id: number;
   user_id: number;
   type: "expense" | "income";
@@ -27,24 +42,35 @@ export type transactionType = {
   deleted: 0 | 1;
 };
 
-export type transactionServiceInputType = Omit<
-  transactionType,
-  "id" | "user_id" | "created_at" | "updated_at" | "deleted"
+export type CreateTransactionServiceInput = Pick<
+  TransactionType,
+  "type" | "amount" | "is_cash" | "account_id" | "category_id"
 > &
-  Partial<Pick<transactionType, "subcategory_id" | "note" | "date">>;
+  Partial<Pick<TransactionType, "subcategory_id" | "note" | "date">>;
 
 export type transactionServiceOutputType = Omit<
-  transactionType,
+  TransactionType,
   "created_at" | "updated_at"
 > & {
   subcategory_id: number | null;
   note: string | null;
 };
 
-export type transactionRepoInputType = Omit<
-  transactionType,
-  "id" | "created_at" | "updated_at" | "deleted" | "subcategory_id" | "note"
+export type CreateTransactionRepoInput = Pick<
+  TransactionType,
+  "user_id" | "type" | "amount" | "is_cash" | "category_id" | "date"
 > & {
+  // > //   "id" | "created_at" | "updated_at" | "deleted" | "subcategory_id" | "note" //   TransactionType, // Omit<
   subcategory_id: number | null;
   note: string | null;
+  account_id: number | null;
+};
+
+export type CreateCashTransactionRepoInput = Pick<
+  TransactionType,
+  "user_id" | "type" | "amount" | "is_cash" | "category_id"
+> & {
+  subcategory_id?: number | null;
+  note?: string | null;
+  date?: Date | null;
 };
