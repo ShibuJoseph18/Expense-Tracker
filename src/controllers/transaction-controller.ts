0b5948457b2d84b357786939a00eae21dec5e600
shibujoseph <shibujoseph1815@gmail.com>
2025-12-27 17:11:05 +0530

    add get transaction controller

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from "express";
-import { createTransactionService } from "../services/transaction-service.js";
+import {
+  createTransactionService,
+  getTransactionService,
+} from "../services/transaction-service.js";
+import { getTransactionSchema } from "../types/transaction-types.js";
 
 export const createTransactionController = async (
   req: Request,
@@ -13,5 +17,30 @@ export const createTransactionController = async (
   res.status(200).json({
     message: "Transaction created successfully",
     transaction: newTransaction,
+  });
+};
+
+export const getTransactionsController = async (
+  req: Request,
+  res: Response
+) => {
+  const validateQueryParams = getTransactionSchema.safeParse(req.query);
+  if (!validateQueryParams.success) {
+    res.status(400).json({
+      message: "Invalid query params key or value is passed",
+      valid_query_params: ["transaction_type", "entity", "offset", "limit"],
+    });
+    return;
+  }
+
+  const transactions = await getTransactionService(
+    req.accessToken.id,
+    validateQueryParams.data
+  );
+
+  res.status(200).json({
+    message: "Transactions fetched successfully",
+    filters: transactions.filters,
+    transaction: transactions.transaction,
   });
 };
